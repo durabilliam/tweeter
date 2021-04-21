558a0b166e26b6972160e223caf9d701f5c2e55e
William Perry <durabilliam@gamil.com>
2021-04-21 21:29:01 -0230

    refetch tweets when submitted

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,40 +8,10 @@ $(document).ready(function() {
   // --- our code goes here ---
  console.log('client');
  
-// $("#btn").on('click', function() {
-//   console.log(this);
-// });
-
-// const $tweet = $(`<article class="tweet">Hello world</article>`);
-// console.log($tweet);
-
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-//]
 
 
-//Create Tweet Element from pseudo obkect database
+
+//Create Tweet Element from pseudo object database
 const createTweetElement = function(tweet) {  
   let markup =`
   <article class="tweet-id">
@@ -78,58 +48,50 @@ const createTweetElement = function(tweet) {
   return markup
 }
 
-//button and serialize data for server!!
+//button and serialize data to send to tweet database
 $("#submit").submit((evt) => {
   evt.preventDefault();
 
-console.log($(evt.target).serialize())
+console.log("HERERERER",$("#tweet-text").val().length)
+const newtweet = $("#tweet-text").val();
+console.log("heahduxge:", newtweet)
+if (newtweet.length > 140){
+  alert("sorry your Tweet is over 140 Characters");
+} else if (newtweet.length === 0){
+  alert("sorry your Tweet is Empty");
+} else {
  $.ajax({
      url: `/tweets`,
-//     url: `http://localhost:8080/tweets   q=${evt.target.search.value}`,
      method: 'POST',
      data: $(evt.target).serialize(),
      dataType: 'text',
    })
-   .then(() => console.log('successfully posted'));
+   .then(() => {console.log('successfully posted');
+       loadTweets()});
+  }
 });
 
-//     dataType: 'JSON'
-//    }).then(function(response) {
-//     console.log(response);
-//     // const item = createItem(response[0])
-//     $('#results').empty();
-//     createItems(response);loadTweets
-// })
 
 //Renders  the tweet elements
 const renderTweets = function(tweets) {
   for (let tweet of tweets) {
-    $('#tweets-container').append(createTweetElement(tweet));
+    $('#tweets-container').prepend(createTweetElement(tweet));
   }
 }
 
+//Fetches Tweets from DataBase With Ajax
 const loadTweets = function() {
   $.ajax({
       url: `/tweets`,
       method: 'GET',
       dataType: 'JSON'
   }).then(function(response) {
-      console.log(response);
       $('#results').empty();
+      console.log("jkhg", response);
       renderTweets(response);
   })
 }
 loadTweets();
-
-
-
-//  $( "form" ).on( "submit", function( event ) {
-//    //event.preventDefault();
-//    console.log( $( this ).serialize() );
-//    debugger
-//  });
-
-
 
 
 
